dfce634dc516e69af9d885adf6636e22f45540eb
Alieu Baldeh <alieu.dev@gmail.com>
2021-08-11 12:57:01 -0400

    completed

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,4 @@
-# require the environment file
-require_relative '../config/enviroment'
+# # require the environment file
+# require_relative '../config/enviroment'
+# require 'bundler/setup'
+# Bundler.require(:default, :development)
